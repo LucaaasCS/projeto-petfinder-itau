78b86cf3daa3952428d4ed07416053ec8b73f133
Lucas Cardoso Silva <100287410+LucaaasCS@users.noreply.github.com>
2024-08-30 18:21:37 -0300

    Feat: Criação do Projeto
    Criação do Projeto contendo as dependências e principais configurações

---
+++ petfinder/src/test/java/com/projeto/petfinder/PetfinderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projeto.petfinder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetfinderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

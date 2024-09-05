e9f06545497eb825be7c613131f8d92e05b86547
Lucas <lucascardoso213@gmail.com>
2024-09-05 20:21:57 -0300

    feat: Criação da Model

---
+++ petfinder/src/main/java/com/projeto/petfinder/model/SensorData.java
@@ -0,0 +1,16 @@
+package com.projeto.petfinder.model;
+
+
+import lombok.*;
+
+@Data
+public class SensorData {
+
+    private String sensorId;
+
+    private String latitude;
+
+    private String longitude;
+
+    private String dataHora;
+}

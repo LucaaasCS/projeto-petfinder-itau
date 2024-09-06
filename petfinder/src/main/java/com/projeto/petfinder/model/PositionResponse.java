9ff7d22be08dbe23f4882773e2463799ce0032ba
Lucas <lucascardoso213@gmail.com>
2024-09-06 00:03:33 -0300

    feat: Correção das models

--- petfinder/src/main/java/com/projeto/petfinder/model/PositionResponse.java
+++ petfinder/src/main/java/com/projeto/petfinder/model/PositionResponse.java
@@ -1,24 +1,9 @@
 package com.projeto.petfinder.model;
 
 import lombok.*;
-
 import java.util.List;
 
 @Data
 public class PositionResponse {
-    private List<Data> data;
-
-    @lombok.Data
-    public static class Data {
-        private String country;
-
-        private String region;
-
-        private String county;
-
-        private String locality;
-
-        private String street;
-
-    }
+    private List<DataStack> data;
 }
